0d22c91b9144e347bb591fe25584c32b51354285
Ssush1 <103404051+Ssush1@users.noreply.github.com>
2022-06-13 13:41:30 +0530

    new

--- snowbirdfrontend/src/Navigation.js
+++ snowbirdfrontend/src/Navigation.js
@@ -4,20 +4,21 @@ import Users from './Users'
 import AddSprint from './AddSprint'
 import Sprints from './Sprints'
 import Task from './Task'
-import Newlogin from './newpro/Newlogin'
-
+// import Newlogin from './newpro/Newlogin'
+import Dash from './Dash'
 function Navigation() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Users />}></Route>
+        <Route path="/" element={<Dash />}></Route>
+          <Route path="/Users" element={<Users />}></Route>
           <Route path="/Sprints" element={<Sprints />}></Route>
           <Route path="/AddSprint" element={<AddSprint />}></Route>
           <Route path="/EditSprint" element={<EditSprint />}></Route>
           <Route path="/Task" element={<Task />}></Route>
-          <Route path="/Newlogin" element={< Newlogin/>}></Route>
-
+          <Route path="/AddProject" element={<Task />}></Route>
+          <Route path="/EditProject" element={<Task />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
