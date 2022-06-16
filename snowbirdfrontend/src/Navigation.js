857837f36e962e0b131613f0ca27385297abc67b
Ssush1 <103404051+Ssush1@users.noreply.github.com>
2022-06-16 09:40:29 +0530

    Addsprint-Sprint2

--- snowbirdfrontend/src/Navigation.js
+++ snowbirdfrontend/src/Navigation.js
@@ -4,6 +4,12 @@ import Users from './Users'
 import AddSprint from './AddSprint'
 import Sprints from './Sprints'
 import Task from './Task'
+import Epic from './Epic'
+import AddProject from './AddProject'
+import EditProject from './EditProject'
+import AddUser from './AddUser'
+import EditUser from './EditUser'
+import Project from './Project'
 // import Newlogin from './newpro/Newlogin'
 import Dash from './Dash'
 function Navigation() {
@@ -16,9 +22,10 @@ function Navigation() {
           <Route path="/Sprints" element={<Sprints />}></Route>
           <Route path="/AddSprint" element={<AddSprint />}></Route>
           <Route path="/EditSprint" element={<EditSprint />}></Route>
+          <Route path="/Epic" element={<Epic />}></Route>
           <Route path="/Task" element={<Task />}></Route>
-          <Route path="/AddProject" element={<Task />}></Route>
-          <Route path="/EditProject" element={<Task />}></Route>
+          <Route path="/AddProject" element={<AddProject />}></Route>
+          <Route path="/EditProject" element={<EditProject/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
